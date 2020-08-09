b5bb622f8353c83cc8534f5b4e2463e4b03f8f79
BenCole6 <benjaminrcole06@gmail.com>
2020-08-09 15:41:47 +1000

    Second_Draft
    Almost there.
    Some LGAs pulsating - not too sure why.
    
    Formatting of plots looking good.

--- Cases LGA Map.R
+++ Cases LGA Map.R
@@ -1,5 +1,3 @@
-# install.packages("rpud")
-
 library(pacman)
 
 p_load(tidyverse,
@@ -62,62 +60,81 @@ for(lga in lgacases_dataframes) {
   }
 }
 
-## Polygon map from LGA borders for all VIC
-#
-# VIC_LGA_map <- ggplot() +
-#   geom_polygon(data = combined_lga_df,
-#                aes(x = long, y = lat,
-#                    group = LGA),
-#                fill = NA,
-#                colour = "grey66",
-#                size = 1)
-
-
 cases_by_LGA_poly <- left_join(combined_lga_df,
                                cases_by_LGA_df,
                                by = "LGA")
 
 cases_by_LGA_poly$DATE <- paste(cases_by_LGA_poly$DATE, "2020")
 
-cases_by_LGA_poly$DATE <- dmy(cases_by_LGA_poly$DATE)
+cases_by_LGA_poly$DATE <- as_date(dmy(cases_by_LGA_poly$DATE),
+                                  format = "%d - %m - %Y")
 
-gg_cases_by_LGA_poly <- ggplot(cases_by_LGA_poly,
-                               aes(x = long, y = lat,
-                                   fill = ACTIVE, group = LGA)) +
-  geom_polygon(colour = "grey66",
-               size = 1) +
-  scale_fill_gradient(low = "grey20", high = "red2",
-                      na.value = "grey80") +
-  labs(title = "Covid cases per LGA by day",
-       subtitle = "{frame_manual}") +
-  coord_equal() +
-  transition_manual(DATE) +
-  theme_void()
+cases_by_LGA_poly <- filter(cases_by_LGA_poly,
+                            !is.na(DATE))
 
-animate(gg_cases_by_LGA_poly,
-        nframes = length(unique(cases_by_LGA_df$DATE)),
-        fps = 4)
-
-anim_save("cases by lga.gif")
-
-ggplot(cases_by_LGA_poly,
-       aes(x = long, y = lat,
-           fill = ACTIVE, group = LGA)) +
+gg_Vic_cases_by_LGA <- ggplot(cases_by_LGA_poly,
+                              aes(x = long, y = lat,
+                                  fill = ACTIVE, group = LGA)) +
   geom_polygon(colour = "grey85",
                size = 1) +
-  scale_fill_gradient(low = "grey66", high = "red2",
-                      na.value = "grey66") +
-  labs(title = " Active Covid-19 Cases per LGA per day",
-       subtitle = "Greater Melbourne Area\n ",
-       caption = "Note: active cases and NOT new cases") +
+  scale_fill_gradient(low = "grey75", high = "red2",
+                      na.value = "grey75") +
+  labs(title = paste(sep = "\n",
+                     "Active Covid-19 Cases per LGA per day",
+                     "{frame_time}"),
+       subtitle = "Victorian Local Government Areas\n\n ",
+       caption = paste(sep = "\n",
+                       "Note: active cases only",
+                       "",
+                       "Cases data:  covidlive.com.au",
+                       "LGA Shape files:  data.vic.gov.au",
+                       "",
+                       "github.com/BenCole6/Covid-Vic-AU")) +
+  coord_equal() +
+  transition_time(DATE) +
+  theme_void() +
+  theme(text = element_text(family = "Arial Nova"),
+        plot.title = element_text(face = "bold", size = 14))
+
+animate(gg_Vic_cases_by_LGA,
+        height = 750, width = 750)
+
+anim_save("Vic Cases by LGA.gif",
+          nframes = length(unique(cases_by_LGA_df$DATE)),
+          fps = 2)
+
+gg_MelbMetro_cases_by_LGA <- ggplot(cases_by_LGA_poly,
+                                    aes(x = long, y = lat,
+                                        fill = ACTIVE, group = LGA)) +
+  geom_polygon(colour = "grey85",
+               size = 0.75) +
+  scale_fill_gradient("Active\nCases",
+                      low = "grey75", high = "red2",
+                      na.value = "grey75") +
+  labs(title = paste(sep = "\n",
+                     "Active Covid-19 Cases per LGA per day",
+                     "{frame_time}"),
+       subtitle = "Greater Melbourne Area\n\n ",
+       caption = paste(sep = "\n",
+                       "Note: active cases only",
+                       "",
+                       "Cases data:  covidlive.com.au",
+                       "LGA Shape files:  data.vic.gov.au",
+                       "",
+                       "github.com/BenCole6/Covid-Vic-AU")) +
   coord_equal(xlim = c(144.10, 145.75),
-              ylim = c(-39.00, -37.25)) +
-  transition_manual(DATE) +
-  theme_void()
+              ylim = c(-38.75, -37.25)) +
+  transition_time(DATE) +
+  theme_void() +
+  theme(text = element_text(family = "Arial Nova"),
+        plot.title = element_text(face = "bold", size = 14))
+
+animate(gg_MelbMetro_cases_by_LGA,
+        height = 750, width = 750)
 
 anim_save("cases by lga greater metro.gif",
           frames = length(unique(cases_by_LGA_poly$DATE)),
           fps = 1,
-          end_pause = 10)
+          end_pause = 30)
 
-beep(2)
+beep(8)
